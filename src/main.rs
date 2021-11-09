115348d5f84933d2e820e206d3a66fd51c1f4e8f
Lily Sturmann <lsturman@redhat.com>
2021-11-08 20:32:15 -0500

    Succeeds

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,15 @@ use std::str::FromStr;
 use tss_esapi::{
     abstraction::{ak, cipher::Cipher, ek, DefaultKey},
     attributes::session::SessionAttributesBuilder,
-    constants::{
-        session_type::SessionType,
-        tss::{TPM2_ALG_NULL, TPM2_ST_ATTEST_QUOTE},
-    },
-    handles::{AuthHandle, KeyHandle, PcrHandle, SessionHandle},
+    constants::session_type::SessionType,
+    handles::AuthHandle,
     interface_types::{
         algorithm::{AsymmetricAlgorithm, HashingAlgorithm, SignatureScheme},
         session_handles::AuthSession,
     },
-    structures::{
-        Digest, DigestValues, EncryptedSecret, IDObject, Name,
-        PcrSelectionList, PcrSelectionListBuilder, PcrSlot,
-    },
     tcti_ldr::TctiNameConf,
-    tss2_esys::{
-        Tss2_MU_TPM2B_PUBLIC_Marshal, Tss2_MU_TPMS_ATTEST_Unmarshal,
-        Tss2_MU_TPMT_SIGNATURE_Marshal, TPM2B_ATTEST, TPM2B_PUBLIC,
-        TPML_DIGEST, TPML_PCR_SELECTION, TPMS_ATTEST, TPMS_SCHEME_HASH,
-        TPMT_SIGNATURE, TPMT_SIG_SCHEME, TPMU_SIG_SCHEME,
-    },
-    utils::{PcrData, Signature},
     Context,
 };
-
-const TSS_MAGIC: u32 = 3135029470;
 
 fn create_empty_session(
     ctx: &mut Context,
@@ -49,70 +33,8 @@ fn create_empty_session(
     session.unwrap()
 }
 
-pub(crate) fn activate_credential(
-    ctx: &mut Context,
-    keyblob: Vec<u8>,
-    ak: KeyHandle,
-    ek: KeyHandle,
-) -> Digest {
-    let (credential, secret) = parse_cred_and_secret(keyblob);
-
-    let ek_auth = create_empty_session(ctx, SessionType::Policy);
-
-    // We authorize ses2 with PolicySecret(ENDORSEMENT) as per PolicyA
-    let _ = ctx.execute_with_nullauth_session(|context| {
-        context.policy_secret(
-            ek_auth.try_into().unwrap(),
-            AuthHandle::Endorsement,
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            None,
-        )
-    }).unwrap();
-
- let resp = ctx
-        .execute_with_sessions(
-            (Some(AuthSession::Password), Some(ek_auth), None),
-            |context| context.activate_credential(ak, ek, credential, secret),
-        ).unwrap();
-
-    ctx.flush_context(ek.into()).unwrap();
-
-    resp
-}
-
-fn parse_cred_and_secret(
-    keyblob: Vec<u8>
-) -> (IDObject, EncryptedSecret) {
-    let magic = u32::from_be_bytes(keyblob[0..4].try_into().unwrap());
-    let version = u32::from_be_bytes(keyblob[4..8].try_into().unwrap());
-
-    if magic != TSS_MAGIC {
-        panic!("Error parsing cred and secret; TSS_MAGIC number does not match expected value");
-    }
-    if version != 1 {
-        panic!("Error parsing cred and secret; version is not 1");
-    }
-
-    let credsize = u16::from_be_bytes(keyblob[8..10].try_into().unwrap());
-    let secretsize = u16::from_be_bytes(
-        keyblob[(10 + credsize as usize)..(12 + credsize as usize)]
-            .try_into()
-            .unwrap(),
-    );
-
-    let credential = &keyblob[10..(10 + credsize as usize)];
-    let secret = &keyblob[(12 + credsize as usize)..];
-
-    let credential = IDObject::try_from(credential).unwrap();
-    let secret = EncryptedSecret::try_from(secret).unwrap();
-
-    (credential, secret)
-}
-
 fn main() {
-    let tcti = TctiNameConf::from_str("/dev/tpmrm0").unwrap();
+    let tcti = TctiNameConf::from_str("device:/dev/tpmrm0").unwrap();
     let mut context = Context::new(tcti).unwrap();
 
     let ek_rsa = ek::create_ek_object(&mut context, AsymmetricAlgorithm::Rsa, DefaultKey).unwrap();
@@ -137,7 +59,6 @@ fn main() {
 
     let (_, key_name, _) = context.read_public(loaded_ak).unwrap();
     let cred = vec![1, 2, 3, 4, 5];
-//    let expected = Digest::try_from(vec![1, 2, 3, 4, 5]).unwrap();
 
      let (credential_blob, secret) = context
         .execute_without_session(|ctx| {
@@ -145,9 +66,24 @@ fn main() {
         })
         .unwrap();
 
-    // keyblob is a vec<u8>
-    let _ = activate_credential(
-            &mut context, credential_blob.to_vec(), loaded_ak, ek_rsa,
-    );
+    let ek_auth = create_empty_session(&mut context, SessionType::Policy);
 
+    let _ = context.execute_with_nullauth_session(|ctx| {
+        ctx.policy_secret(
+            ek_auth.try_into().unwrap(),
+            AuthHandle::Endorsement,
+            Default::default(),
+            Default::default(),
+            Default::default(),
+            None,
+        )
+    }).unwrap();
+
+    let _ = context
+        .execute_with_sessions(
+            (Some(AuthSession::Password), Some(ek_auth), None),
+            |context| context.activate_credential(loaded_ak, ek_rsa, credential_blob, secret),
+        ).unwrap();
+
+    println!("Success!");
 }
